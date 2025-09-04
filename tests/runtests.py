2f1527cd2ff174ffbafb9bcd5595e4d2368ace8a
Emanuele Di Giacomo <edigiacomo@arpae.it>
2025-09-04 11:27:26 +0200

    Add an example test to avoid failing check in spec file

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,9 +1,11 @@
 import unittest
 
 
-# class TestExample(unittest.TestCase):
-#     def test_somethins(self):
-#         self.assertEqual(1, 1, "1 = 1")
+# This is a unittest example. It can be used as a template for concrete tests and it prevents unittest.test to exit with
+# status 5 (no test run).
+class TestExample(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(1, 1, "1 = 1")
 
 
 if __name__ == '__main__':
